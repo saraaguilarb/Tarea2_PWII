d6cf5963e2c64e2efd09c2c85420e0ad7c18a5ce
saraaguilarb <122842897+saraaguilarb@users.noreply.github.com>
2023-01-31 22:17:26 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
 const empresas = []; //colección
 
 class Empresa {
-    constructor(nombre, pais_origen) {
+    constructor(nombre, pais_origen, lanzamiento) {
         this.nombre = nombre;
         this.pais_origen = pais_origen;
+        this.lanzamiento = lanzamiento;
     }
 }
 
-const toyota = new Empresa("Toyota", "Japón");
-const volvo = new Empresa("Volvo", "Italia");
+const toyota = new Empresa("Toyota", "Japón", "1958");
+const volvo = new Empresa("Volvo", "Italia", "1965");
 
 empresas.push(toyota);
 empresas.push(volvo);
+
+delete empresas[0].lanzamiento; // eliminar propiedad del elemento
+empresas[0].lanzamiento = "1940"; // creando propiedad para el elemento
+
 
 console.log(empresas[0]);
 console.log(empresas[1]);
